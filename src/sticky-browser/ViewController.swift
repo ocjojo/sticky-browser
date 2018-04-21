26dfa22a87e43a9c3439b4963095c36c14ab8e3a
Lukas Ehnle <l.ehnle@web.de>
2018-04-21 03:47:27 +0200

    working webview

--- src/sticky-browser/ViewController.swift
+++ src/sticky-browser/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, WKNavigationDelegate, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: view.frame, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        
+        let url = URL(string: "https://www.netflix.com/browse")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        
+        view.addSubview(webView)
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        print(error.localizedDescription)
+    }
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("start to load")
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("finish to load")
     }
 
 
