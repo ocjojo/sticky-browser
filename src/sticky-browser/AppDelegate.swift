066514a5dd080ab2b7778d3b207d3e5953379db2
Lukas Ehnle <l.ehnle@web.de>
2018-04-21 02:10:50 +0200

    initial swift project

---
+++ src/sticky-browser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sticky-browser
+//
+//  Created by Lukas Ehnle on 21.04.18.
+//  Copyright © 2018 Lukas Ehnle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ src/sticky-browser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sticky-browser
+//
+//  Created by Lukas Ehnle on 21.04.18.
+//  Copyright © 2018 Lukas Ehnle. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
